c311f1180949c12ce04b84bf32547fda43eb0304
Satoshi Nagashiba <bobtabo.buhibuhi@gmail.com>
2021-03-21 03:18:30 +0900

    ビュー修正

--- src/components/create.vue
+++ src/components/create.vue
@@ -1,101 +1,88 @@
 <template>
-  <header>
-    <div class="navbar navbar-dark bg-dark shadow-sm">
-      <div class="container d-flex justify-content-between">
-        <a href="src/components/index.vuex.vue" class="navbar-brand d-flex align-items-center">
-          <strong>顧客管理（新規登録）</strong>
-        </a>
-      </div>
+  <div class="container-fluid" style="margin-top: 50px; padding-left: 100px;padding-right: 100px;">
+    <div class="alert alert-danger" role="alert">
+      【メッセージサンプル】エラーです。
     </div>
-  </header>
 
-  <main role="main">
-    <div class="container-fluid" style="margin-top: 50px; padding-left: 100px;padding-right: 100px;">
-      <div class="alert alert-danger" role="alert">
-        【メッセージサンプル】エラーです。
-      </div>
-
-      <form id="form" method="post" action="src/components/index.vuex.vue">
-        <div class="col-md-8 order-md-1">
-          <div class="row">
-            <div class="col-md-3 mb-3">
-              <label>姓 <span class="badge badge-danger">必須</span></label>
-              <input type="text" class="form-control" name="last_name" placeholder="姓" value="" required>
-            </div>
-            <div class="col-md-3 mb-3">
-              <label>名 <span class="badge badge-danger">必須</span></label>
-              <input type="text" class="form-control" name="first_name" placeholder="名" value="" required>
-            </div>
+    <form id="form" method="post" action="src/components/index.vuex.vue">
+      <div class="col-md-8 order-md-1">
+        <div class="row">
+          <div class="col-md-3 mb-3">
+            <label>姓 <span class="badge badge-danger">必須</span></label>
+            <input type="text" class="form-control" name="last_name" placeholder="姓" value="" required>
           </div>
-
-          <div class="row">
-            <div class="col-md-3 mb-3">
-              <label>姓かな <span class="badge badge-danger">必須</span></label>
-              <input type="text" class="form-control" name="last_kana" placeholder="姓かな" value="" required>
-            </div>
-            <div class="col-md-3 mb-3">
-              <label>名かな <span class="badge badge-danger">必須</span></label>
-              <input type="text" class="form-control" name="first_kana" placeholder="名かな" value="" required>
-            </div>
+          <div class="col-md-3 mb-3">
+            <label>名 <span class="badge badge-danger">必須</span></label>
+            <input type="text" class="form-control" name="first_name" placeholder="名" value="" required>
           </div>
+        </div>
 
-          <div class="row">
-            <div class="col-md-3 mb-3">
-              <label>性別 <span class="badge badge-danger">必須</span></label>
-              <div class="col-sm-10 text-left">
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="gender" value="1">
-                  <label class="form-check-label">男</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="gender" value="2">
-                  <label class="form-check-label">女</label>
-                </div>
+        <div class="row">
+          <div class="col-md-3 mb-3">
+            <label>姓かな <span class="badge badge-danger">必須</span></label>
+            <input type="text" class="form-control" name="last_kana" placeholder="姓かな" value="" required>
+          </div>
+          <div class="col-md-3 mb-3">
+            <label>名かな <span class="badge badge-danger">必須</span></label>
+            <input type="text" class="form-control" name="first_kana" placeholder="名かな" value="" required>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-3 mb-3">
+            <label>性別 <span class="badge badge-danger">必須</span></label>
+            <div class="col-sm-10 text-left">
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="gender" value="1">
+                <label class="form-check-label">男</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="gender" value="2">
+                <label class="form-check-label">女</label>
               </div>
             </div>
           </div>
+        </div>
 
-          <div class="row">
-            <div class="col-md-2 mb-3">
-              <label>郵便番号 <span class="badge badge-danger">必須</span></label>
-              <input type="text" class="form-control" name="post_code" placeholder="郵便番号" value="" required>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-5 mb-3">
-              <label>住所 <span class="badge badge-danger">必須</span></label>
-              <input type="text" class="form-control" name="address" placeholder="渋谷区道玄坂2丁目11-1" value="" required>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-5 mb-3">
-              <label>建物名</label>
-              <input type="text" class="form-control" name="building" placeholder="Ｇスクエア渋谷道玄坂 4F" value="">
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-3 mb-3">
-              <label>メールアドレス <span class="badge badge-danger">必須</span></label>
-              <input type="email" class="form-control" name="email" placeholder="you@example.com" value="" required>
-            </div>
+        <div class="row">
+          <div class="col-md-2 mb-3">
+            <label>郵便番号 <span class="badge badge-danger">必須</span></label>
+            <input type="text" class="form-control" name="post_code" placeholder="郵便番号" value="" required>
           </div>
         </div>
-      </form>
-      <hr class="mb-4">
-      <div class="form-group">
-        <router-link class="btn btn-secondary" :to="{ name: 'index' }" style="width:150px">戻る</router-link>
-        <button id="complete" type="button" class="btn btn-success" style="width:150px"><i class="fas fa-database pr-1"></i> 登録</button>
+
+        <div class="row">
+          <div class="col-md-5 mb-3">
+            <label>住所 <span class="badge badge-danger">必須</span></label>
+            <input type="text" class="form-control" name="address" placeholder="渋谷区道玄坂2丁目11-1" value="" required>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-5 mb-3">
+            <label>建物名</label>
+            <input type="text" class="form-control" name="building" placeholder="Ｇスクエア渋谷道玄坂 4F" value="">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-3 mb-3">
+            <label>メールアドレス <span class="badge badge-danger">必須</span></label>
+            <input type="email" class="form-control" name="email" placeholder="you@example.com" value="" required>
+          </div>
+        </div>
       </div>
+    </form>
+    <hr class="mb-4">
+    <div class="form-group">
+      <router-link class="btn btn-secondary" :to="{ name: 'index' }" style="width:150px">戻る</router-link>
+      <button id="complete" type="button" class="btn btn-success" style="width:150px"><i class="fas fa-database pr-1"></i> 登録</button>
     </div>
-  </main>
 
-  <div id="complete-confirm" title="確認" style="display: none;">
-    <p><span class="ui-icon ui-icon-info" style="float:left; margin:12px 12px 20px 0;"></span>登録しますか？</p>
+    <div id="complete-confirm" title="確認" style="display: none;">
+      <p><span class="ui-icon ui-icon-info" style="float:left; margin:12px 12px 20px 0;"></span>登録しますか？</p>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -111,8 +98,12 @@ export default {
 
     function completeConfirm(response){
       var buttons = {};
-      buttons['キャンセル'] = function(){$(this).dialog('close');response(false)};
-      buttons['登録'] = function(){$(this).dialog('close');response(true)};
+      buttons['キャンセル'] = function(){
+        $(this).dialog('close');response(false);
+      };
+      buttons['登録'] = function(){
+        $(this).dialog('close');response(true);
+      };
 
       $("#complete-confirm").dialog({
         show: {
@@ -131,5 +122,5 @@ export default {
       });
     }
   }
-}
+};
 </script>
--- src/components/detail.vue
+++ src/components/detail.vue
@@ -1,84 +1,72 @@
 <template>
-  <header>
-    <div class="navbar navbar-dark bg-dark shadow-sm">
-      <div class="container d-flex justify-content-between">
-        <a href="src/components/index.vuex.vue" class="navbar-brand d-flex align-items-center">
-          <strong>顧客管理（詳細）</strong>
-        </a>
+  <div class="container-fluid" style="margin-top: 50px; padding-left: 100px;padding-right: 100px;">
+    <div class="col-md-8 order-md-1">
+      <div class="row">
+        <div class="col-md-3 mb-3">
+          <label>姓</label>
+          <input type="text" class="form-control" name="last_name" placeholder="姓" value="苗字" readonly>
+        </div>
+        <div class="col-md-3 mb-3">
+          <label>名</label>
+          <input type="text" class="form-control" name="first_name" placeholder="名" value="名前" readonly>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-3 mb-3">
+          <label>姓かな</label>
+          <input type="text" class="form-control" name="last_kana" placeholder="姓かな" value="みょうじ" readonly>
+        </div>
+        <div class="col-md-3 mb-3">
+          <label>名かな</label>
+          <input type="text" class="form-control" name="first_kana" placeholder="名かな" value="なまえ" readonly>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-3 mb-3">
+          <label>性別</label>
+          <input type="text" class="form-control" name="gender" value="男" readonly>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-2 mb-3">
+          <label>郵便番号</label>
+          <input type="text" class="form-control" name="post_code" placeholder="郵便番号" value="123-4567" readonly>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-5 mb-3">
+          <label>住所</label>
+          <input type="text" class="form-control" name="address" placeholder="渋谷区道玄坂2丁目11-1" value="青森市長島一丁目1-1" readonly>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-5 mb-3">
+          <label>建物名</label>
+          <input type="text" class="form-control" name="building" placeholder="Ｇスクエア渋谷道玄坂 4F" value="" readonly>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-3 mb-3">
+          <label>メールアドレス</label>
+          <input type="email" class="form-control" name="email" placeholder="you@example.com" value="bobtabo.buhibuhi@gmail.com" readonly>
+        </div>
       </div>
     </div>
-  </header>
-
-  <main role="main">
-    <div class="container-fluid" style="margin-top: 50px; padding-left: 100px;padding-right: 100px;">
-      <div class="col-md-8 order-md-1">
-        <div class="row">
-          <div class="col-md-3 mb-3">
-            <label>姓</label>
-            <input type="text" class="form-control" name="last_name" placeholder="姓" value="苗字" readonly>
-          </div>
-          <div class="col-md-3 mb-3">
-            <label>名</label>
-            <input type="text" class="form-control" name="first_name" placeholder="名" value="名前" readonly>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-3 mb-3">
-            <label>姓かな</label>
-            <input type="text" class="form-control" name="last_kana" placeholder="姓かな" value="みょうじ" readonly>
-          </div>
-          <div class="col-md-3 mb-3">
-            <label>名かな</label>
-            <input type="text" class="form-control" name="first_kana" placeholder="名かな" value="なまえ" readonly>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-3 mb-3">
-            <label>性別</label>
-            <input type="text" class="form-control" name="gender" value="男" readonly>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-2 mb-3">
-            <label>郵便番号</label>
-            <input type="text" class="form-control" name="post_code" placeholder="郵便番号" value="123-4567" readonly>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-5 mb-3">
-            <label>住所</label>
-            <input type="text" class="form-control" name="address" placeholder="渋谷区道玄坂2丁目11-1" value="青森市長島一丁目1-1" readonly>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-5 mb-3">
-            <label>建物名</label>
-            <input type="text" class="form-control" name="building" placeholder="Ｇスクエア渋谷道玄坂 4F" value="" readonly>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-3 mb-3">
-            <label>メールアドレス</label>
-            <input type="email" class="form-control" name="email" placeholder="you@example.com" value="bobtabo.buhibuhi@gmail.com" readonly>
-          </div>
-        </div>
-      </div>
-      <hr class="mb-4">
-      <div class="form-group">
-        <router-link class="btn btn-secondary" :to="{ name: 'index' }" style="width:150px">戻る</router-link>
-        <button id="complete" type="button" class="btn btn-danger" style="width:150px"><i class="fas fa-database pr-1"></i> 削除</button>
-      </div>
+    <hr class="mb-4">
+    <div class="form-group">
+      <router-link class="btn btn-secondary" :to="{ name: 'index' }" style="width:150px">戻る</router-link>
+      <button id="complete" type="button" class="btn btn-danger" style="width:150px"><i class="fas fa-database pr-1"></i> 削除</button>
     </div>
-  </main>
 
-  <div id="complete-confirm" title="確認" style="display: none;">
-    <p><span class="ui-icon ui-icon-info" style="float:left; margin:12px 12px 20px 0;"></span>削除しますか？</p>
+    <div id="complete-confirm" title="確認" style="display: none;">
+      <p><span class="ui-icon ui-icon-info" style="float:left; margin:12px 12px 20px 0;"></span>削除しますか？</p>
+    </div>
   </div>
 </template>
 
@@ -95,8 +83,12 @@ export default {
 
     function completeConfirm(response){
       var buttons = {};
-      buttons['キャンセル'] = function(){$(this).dialog('close');response(false)};
-      buttons['削除'] = function(){$(this).dialog('close');response(true)};
+      buttons['キャンセル'] = function(){
+        $(this).dialog('close');response(false);
+      };
+      buttons['削除'] = function(){
+        $(this).dialog('close');response(true);
+      };
 
       $("#complete-confirm").dialog({
         show: {
@@ -115,5 +107,5 @@ export default {
       });
     }
   }
-}
+};
 </script>
